a21ca4998e71f9310964146fade1a4fcfd6c67dc
Devesh_singh <choudharydevesh121@gmail.com>
2024-05-08 19:52:55 +0530

    corrected profile image and techstack image

--- frontend/src/pages/TechStack.jsx
+++ frontend/src/pages/TechStack.jsx
@@ -7,7 +7,7 @@ const TechStack = () => {
 					<h1 className="text-4xl font-bold pb-2">My Tech Stack</h1>
 					<h2 className="text-xl opacity-85"> Technologies I’ve been working with recently</h2>
 				</div>
-				<div className=" bg-[url('../../public/FullStack_bg.png')] bg-cover bg-center bg-no-repeat w-full h-[80%] relative mt-10 rounded-full shadow-5xl opacity-90 ">
+				<div className=" bg-[url('/FullStack_bg.png')] bg-cover bg-center bg-no-repeat w-full h-[80%] relative mt-10 rounded-full shadow-5xl opacity-90 ">
 				</div>
 
 			</div >
